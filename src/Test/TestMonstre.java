66025dafbd70c4b3fc335e60ce6fc7147514d472
Maxence.M <duskorm.prod@gmail.com>
2021-06-08 11:22:10 +0200

    ajout modif testMonstre

--- src/Test/TestMonstre.java
+++ src/Test/TestMonstre.java
@@ -7,8 +7,17 @@ import org.junit.Test;
 public class TestMonstre {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreationMonstre() {
+		//on créé un jeu avec le constructeur par défaut
+		JeuPerso j = new JeuPerso();
+
+		//on prend la liste de monstre de ce jeu créé
+		ArrayList<Monstre> l = j.getListeMonstre();
+
+		//on prend le nombre de monstre qui ont été créé par ce jeu
+		int res = l.size();
+		
+		assertEquals("le jeu devrait créer un monstre seulement",1,res);
 	}
 
 }
